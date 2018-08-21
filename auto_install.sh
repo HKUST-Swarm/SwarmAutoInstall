93efff63aea915e1c4795daf230577f8e9727a29
XuHao <xuhao1@users.noreply.github.com>
2018-08-21 20:24:18 +0800

    Fix name issue

--- auto_install.sh
+++ auto_install.sh
@@ -8,7 +8,7 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /u
 sudo update-alternatives --config gcc
 
 ping dl.dropboxusercontent.com -c 5
-wget https://dl.dropboxusercontent.com/s/hrq71iwxcuqzkja/SwarmDepends.tgz?dl=0
+wget https://dl.dropboxusercontent.com/s/hrq71iwxcuqzkja/SwarmDepends.tgz
 tar -xf SwarmDepends.tgz -C ~/
 #Seems need to Uninstall old eigen3 first 
 
