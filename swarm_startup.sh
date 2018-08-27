dfa7bab1a4c56f2fdcccd076c2b84fa9900e3429
XuHao <xuhao1@users.noreply.github.com>
2018-08-27 17:43:55 +0800

    Default enable chicken blood mode

--- swarm_startup.sh
+++ swarm_startup.sh
@@ -1,5 +1,6 @@
 /usr/bin/env bash
 echo "Start ros"
 sudo -S sh -c 'echo 2048 > /sys/module/usbcore/parameters/usbfs_memory_mb'
+/home/dji/jetson_clocks.sh
 /bin/bash /home/dji/SwarmAutoInstall/start_ros.sh &
 
