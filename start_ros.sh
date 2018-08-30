ac4721e165f37f05e588a119c0debb1cf3ed746b
xuhao1 <xuhao3e8@gmail.com>
2018-08-30 14:13:49 +0000

    Modified init camera

--- start_ros.sh
+++ start_ros.sh
@@ -8,13 +8,13 @@ export ROS_MASTER_URI=http://localhost:11311
 roscore > /home/dji/log_roscore.txt&
 sleep 5
 echo "Start uwb"
-roslaunch ptgrey_reader stereo-unsync.launch &
+roslaunch swarm_vo_fuse stereo.launch is_sync:=false > /home/dji/log_swarm.txt &
 PG_PID=$!
 
 sleep 10
 kill -9 $PG_PID
 
-roslaunch swarm_vo_fuse swarm_vo_fuse.launch > /home/dji/log_swarm.txt &
+roslaunch swarm_vo_fuse swarm_vo_fuse.launch >> /home/dji/log_swarm.txt &
 sleep 30
 roslaunch cascade_controller mf2_swarm.launch > /home/dji/log_swarm_controller &
 /bin/bash /home/dji/SwarmAutoInstall/start_controller.sh
