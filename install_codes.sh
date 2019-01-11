3bca33c7f0846b762e01dc88e07edb60b06d6143
XuHao <xuhao1@users.noreply.github.com>
2019-01-11 21:35:39 +0800

    Don't need mocka anymore

--- install_codes.sh
+++ install_codes.sh
@@ -13,7 +13,7 @@ git clone https://github.com/xuhao1/ptgrey_reader.git
 git clone https://xuhao1:$github_pwd@github.com/xuhao1/swarm_pkgs.git
 git clone https://github.com/xuhao1/camera_calibration_frontend.git
 git clone -b UltraFast https://xuhao1:$github_pwd@github.com/HKUST-Aerial-Robotics/VINS_Stereo.git
-git clone https://xuhao1:$github_pwd@github.com/xuhao1/mocka.git
+#git clone https://xuhao1:$github_pwd@github.com/xuhao1/mocka.git
 
 cd $SWARM_WS/src/ptgrey_reader
 sudo ./script/autoinstall_tx2.sh
